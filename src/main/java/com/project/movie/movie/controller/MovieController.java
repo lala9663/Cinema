2e8cae5757e77bb39681345cfaeae8725dbf343d
lala9663 <lala96632040@gmail.com>
2023-09-15 21:15:43 +0900

    refactor: 오타 수정

--- src/main/java/com/project/movie/movie/controller/MovieController.java
+++ src/main/java/com/project/movie/movie/controller/MovieController.java
@@ -22,7 +22,7 @@ public class MovieController {
     public ResponseEntity<?> registerMovie(@RequestBody RegisterMovieDto registerMovieDto) {
         try {
             Long movieId = movieService.registerMovie(registerMovieDto);
-            return ResponseEntity.ok("영화가 등록되었습니다. 영화 ID는 S" + movieId + " 입니다.");
+            return ResponseEntity.ok("영화가 등록되었습니다. 영화 ID는 " + movieId + " 입니다.");
         } catch (DataAccessException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("등록 실패 했습니다.");
         }
