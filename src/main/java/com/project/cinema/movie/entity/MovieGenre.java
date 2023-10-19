92020443296335e633904504b8aa60619929c8f2
lala9663 <lala96632040@gmail.com>
2023-10-19 15:34:51 +0900

    refactor: 영화에 있는 enum 좀더 간결하게 변경

--- src/main/java/com/project/cinema/movie/entity/MovieGenre.java
+++ src/main/java/com/project/cinema/movie/entity/MovieGenre.java
@@ -1,10 +1,8 @@
 package com.project.cinema.movie.entity;
 
 public enum MovieGenre {
-    액션,
-    코미디,
-    로맨스,
-    스릴러,
-    판타지,
-    다큐,
+    ACTION,
+    COMEDY,
+    ROMANCE,
+    FANTASY
 }
--- src/main/java/com/project/cinema/movie/entity/MovieRate.java
+++ src/main/java/com/project/cinema/movie/entity/MovieRate.java
@@ -3,7 +3,7 @@ package com.project.cinema.movie.entity;
 public enum MovieRate {
     G,
     PG,
-    PG_13,
+    PG13,
     R,
-    NC_17
+    NC17
 }
